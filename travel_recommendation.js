c164c7ed8b596ca6c6eb0d565ed6c628cdbc223b
Sean Kim <sean.siwon.kim@gmail.com>
2025-09-01 11:34:20 -0400

    final touches

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -28,24 +28,88 @@ document.getElementById("search").addEventListener("click", async () => {
     if(query === "beach") {
         const beachesArray = data.beaches;
 
-        for(let i=0; i<beachesArray; i++) {
+        for(let i=0; i<beachesArray.length; i++) {
             const infoPop = document.createElement("div");
             infoPop.classList.add("infoPop");
             searchResults.appendChild(infoPop);
 
             const img = document.createElement("img");
-            const name = document.createElement("h3");
-            const desc = document.createElement("p");
-            const but = document.createElement("button");
+            img.src = beachesArray[i].imageUrl;
 
+            const name = document.createElement("h3");
+            name.textContent = beachesArray[i].name;
+
+            const desc = document.createElement("p");
+            desc.textContent = beachesArray[i].description;
+
+            const but = document.createElement("button");
+            but.textContent = "Visit";
+
+            infoPop.appendChild(img);
+            infoPop.appendChild(name);
+            infoPop.appendChild(desc);
+            infoPop.appendChild(but);
         }
 
     } else if(query === "temple") {
+        const templesArray = data.temples;
 
+        for(let i=0; i<templesArray.length; i++) {
+            const infoPop = document.createElement("div");
+            infoPop.classList.add("infoPop");
+            searchResults.appendChild(infoPop);
+
+            const img = document.createElement("img");
+            img.src = templesArray[i].imageUrl;
+
+            const name = document.createElement("h3");
+            name.textContent = templesArray[i].name;
+
+            const desc = document.createElement("p");
+            desc.textContent = templesArray[i].description;
+
+            const but = document.createElement("button");
+            but.textContent = "Visit";
+
+            infoPop.appendChild(img);
+            infoPop.appendChild(name);
+            infoPop.appendChild(desc);
+            infoPop.appendChild(but);
+        }
     } else if(query === "country") {
+        const countryArray = data.countries;
 
+        for(let i=0; i<countryArray.length; i++) {
+            citiesArray = countryArray[i].cities;
+
+            for(let j=0; j<citiesArray.length; j++) {
+                const infoPop = document.createElement("div");
+                infoPop.classList.add("infoPop");
+                searchResults.appendChild(infoPop);
+
+                const img = document.createElement("img");
+                img.src = citiesArray[j].imageUrl;
+
+                const name = document.createElement("h3");
+                name.textContent = citiesArray[j].name;
+
+                const desc = document.createElement("p");
+                desc.textContent = citiesArray[j].description;
+
+                const but = document.createElement("button");
+                but.textContent = "Visit";
+
+                infoPop.appendChild(img);
+                infoPop.appendChild(name);
+                infoPop.appendChild(desc);
+                infoPop.appendChild(but);
+            }
+        }
     } else {
-        console.log("No Match Found.");
+        alert("No Match Found");
     }
 })
 
+document.getElementById("clear").addEventListener("click", () => {
+    document.querySelector(".searchResults").innerHTML = "";
+})
